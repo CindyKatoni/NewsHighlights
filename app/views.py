6c101a0b6b807dcde74ce9690ef64418a53fda2c
CindyKatoni <katmuema@gmail.com>
2020-09-12 19:41:20 +0300

    adadded navigation menu

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 # Create put index page view function
 # Views
 @app.route('/')
+@app.route('/home')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route("/article")    
+def article():
+    return render_template('article.html')    
